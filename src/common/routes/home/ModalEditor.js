d2d32be8b0347076639d8e8a11f20a340799fade
Jayaram Hanumanthappa <jhanumanthappa@infinera.com>
2022-11-05 11:41:34 +0530

    Improve resizing

--- src/common/routes/home/ModalEditor.js
+++ src/common/routes/home/ModalEditor.js
@@ -62,6 +62,18 @@ class ModalEditor extends React.Component {
             if (this.props.cmRef) {
                 this.props.cmRef.ref = this.codeMirror;
             }
+            this.codeMirror.on("keyup", function (cm, e) {
+                if (me.codeMirror.getDoc().lineCount() === 1)
+                    h = 25;
+                else 
+                    h = (me.codeMirror.getDoc().lineCount() + 10) * 18;
+                //if (h > vh) h = vh;
+                me.codeMirror.setSize("100%", `${h}px`);
+                // refresh() was jarring the view sometimes. The scrollIntoView
+                // seems to be much more smoother. 
+                me.codeMirror.scrollIntoView(me.codeMirror.getDoc().getCursor(), 10);
+                //me.codeMirror.refresh();
+            });    
             this.codeMirror.on("keydown", function (cm, e) {
                 switch (e.keyCode) {
                     case 13:
